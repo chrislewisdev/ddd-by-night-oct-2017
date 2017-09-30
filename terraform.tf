8ad449e31d59596bd01c0771189f3cde09a4480b
Chris Lewis <cl_myles@hotmail.com>
2017-09-30 14:17:52 +1000

    Comments and instructions to help getting started with this terraform file

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,28 @@
+#We'll be deploying into the Sydney region of AWS.
 provider "aws" {
   region = "ap-southeast-2"
 }
 
 variable "website_name" {
     type = "string"
+    description = "Name of the S3 bucket and website to create, e.g. 'hello-world.com'"
+
+    #####################################
+    #IMPORTANT: Set this to the name you want for the S3 bucket that our terraform will create!
+    #####################################
     default = "hello-world.com"
 }
 
 terraform {
   backend "s3" {
-    bucket = "chrislewisdev-terraform"
     key    = "ddd-by-night-oct-2017/terraform.tfstate"
     region = "ap-southeast-2"
+    
+    #####################################
+    #IMPORTANT: Set this to the name of an EXISTING S3 bucket in your AWS account.
+    #Terraform will use this to store information about your infrastructure so it knows what to create/update/delete when you apply changes.
+    #####################################
+    bucket = "chrislewisdev-terraform"
   }
 }
 
@@ -23,6 +34,7 @@ data "template_file" "s3_website_policy" {
   }
 }
 
+#This creates our bucket with a public access policy and minimal website config.
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "${var.website_name}"
   acl    = "public-read"
@@ -33,6 +45,8 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
+#Once terraform has completed, it will display the value of this output variable.
+#You can access this URL to see your deployed website!
 output "website_url" {
   value = "${aws_s3_bucket.website_bucket.website_endpoint}"
 }
